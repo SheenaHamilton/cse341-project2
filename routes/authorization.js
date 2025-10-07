a6653f02d744487e6a8cfc3c3c90af0ac8ea672d
sheenahamilton <sheenhamilton@gmail.com>
2025-10-07 00:17:05 -0600

    fix github scope

--- routes/authorization.js
+++ routes/authorization.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../config/passport');
 
-router.get('/login', passport.authenticate('github'), (req, res) => { scope: ['user:email'] });
+router.get('/login', passport.authenticate('github', { scope: ['user:email'] }));
 
 router.get('/logout', function (req, res, next) {
     req.logout(function (err) {
